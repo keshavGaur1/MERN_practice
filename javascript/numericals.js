551aa77d60cbfb5ed040d82e778963d7bb14b8ee
Keshav Gaur <keshavgaur28@gmail.com>
2025-07-24 09:23:04 +0530

    js start mongoDB completed

---
+++ javascript/numericals.js
@@ -0,0 +1,85 @@
+/*
+
+--------------------------------------------------------------------------------------------
+
+let a = 5 
+let b = "5"  
+
+console.log(a+b)   // 55
+console.log(a-b)   // 0
+console.log(a*b)   // 25
+console.log(a/b)   // 1
+
+sirf + ke case mai 
+
+--------------------------------------------------------------------------------------------
+
+let a=5
+let b="5"
+
+console.log( a + (a>b) )  
+ans - 5
+
+how ??
+
+console.log( 5 + false )
+console.log( 5 + 0 )
+ 
+
+let a="5"
+let b=5
+
+console.log( a + (a==b) )  
+ans - 5true
+
+how ??
+
+console.log( "5" + true )
+
+
+--------------------------------------------------------------------------------------------
+
+
+
+    let a=5 , b=6 ,c=7 ;
+
+    let result ;
+    result = (a+7) = (b-3) = (c+4)
+
+    console.log(result)   
+
+    // ye error dega - Uncaught SyntaxError: Invalid left-hand side in assignment
+
+    assignment operation mai LHS mai koi variable hona chiye tabhi to usko value assign krege
+
+    assignment operator ke LHS mai - agar expression ya constant hai to error hi ayegi
+
+
+
+--------------------------------------------------------------------------------------------
+
+
+
+
+    let a=5 , b=6 ,c=7 ;
+
+    let result ;
+    result = a = b = (c+4)
+
+    console.log(result)   // 11 
+
+
+
+--------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+*/
--- mongoDB/database_backup.js
+++ mongoDB/database_backup.js
@@ -2,29 +2,118 @@
 
 download mongodb-database-tools
 
-mongodump --version  // ab mongodump chalna chiye (uska version pata chalega)
+mongodump --version  // ab mongodump chalna chiye (uska version pata chalega)\
 
-1) backup of complete dababase
+
+
+// --------------------------------------------------------------------------------------------------------------------------------------
+1) backup of complete dababase in binary format
+
+using mongodump - 
+
+check version - mongodump --version
+
+syntax - mongodump --db {DB_NAME} --out {LOCATION}
+
+DB_NAME - database jiska backup apni machine per lena hai
+LOCATION - jaha ye database apni machine per store krana hai
+
 
 mongodump --db allenhouse_db --out c:\
 
-mongodump --db databse_name --out c:\location
+
+imp note - jab database ka backup lo mongodump se to -- data json ke binary format mai aata hai
 
 
 
 
 
 
+// ---------------------------------------------------------------------------------------------------------------------------------
+
+2) restore the database - save backup of database present in binary format in new mongoDB database
+
+using mongoexport - 
+
+check version - mongoexport --version
+
+syntax - mongorestore --db {DB_NAME} --collection {COLLECTION_NAME} {LOCATION}
+
+DB_NAME - name of new database jisme hme computer se data laake store krana hai
+COLLECTION_NAME - database DB_NAME mai konse collection ko restore krana tha .
+LOCATION - location of database jo hmare computer mai save hai aur ise restore krana hai
+
+
+mongorestore --db restoredDB --collection DatesDemo C:\allenhouse_db\DatesDemo.bson
+
+
+
+
+//----------------------------------------------------------------------------------------------
+
+3)  backup of complete dababase in json or .csv format
+
+using mongoexport - 
+
+check version - mongoexport --version
+
+syntax - mongoexport --db={DB_NAME} --collection={COLLECTION_NAME} --out={LOCATION}
+
+DB_NAME - jis database ka backup lena h apne computer per
+COLLECTION_NAME - jis database ka backup lena h apne computer per
+LOCATION - location jaha data store ho 
+
+
+// d drive mai normally export ho jayega data (database se backup)
+mongoexport --db=employees --collection=managers --out=d:\\managers.json
+
+// c drive mai backup ke liye - cmd ko run as administrator krna pdega
+mongoexport --db=employees --collection=managers --out=c:\managers.csv
 
 
 
 
 
 
+//----------------------------------------------------------------------------------------------
+
+4) Import data (which is saved in csv or json format) to database 
+
+using mongoimport -
+
+check version - mongoimport --version
+
+syntax - mongoimport --db={DB_NAME} --collection={NEW_COLLECTION} --file={LOCATION/FILE_NAME.TYPE} --type={TYPE}
+
+
+DB_NAME - jis database mai import krana hai
+NEW_COLLECTION - jo collection ka naam chaiye ( ki iss name ke collection mai data store ho)
+LOCATION - location where the file is stored ( jaha file save hai ) 
+FILE_NAME - jis naam se file save hai aur isi ka backup lena hai
+TYPE - .json or .csv
+
+
+mongoimport --db=test --collection=manager_coll --file=D:\managers.json --type=json
 
 
 
+
+
+// ===========================================================================================================
+
+1) In binary format (BSON)
+
+a) backup : mongodump
+b) restore : mongorestore
+
+
+2) In JSON/CSV format 
+
+a) backup : mongoexport
+b) restore : mongoimport
 
 
 
 */
+
+
