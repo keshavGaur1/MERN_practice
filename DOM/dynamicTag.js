e58f770de52947f4734a6dc0a6025b8b1aebd341
Keshav Gaur <keshavgaur28@gmail.com>
2025-07-29 12:40:40 +0530

    dom done

---
+++ DOM/dynamicTag.js
@@ -0,0 +1,16 @@
+/*
+
+document.createElement('h1')  - to create new tag / element dynamically
+
+document.body.append(myH); - body mai append krane ke liye ( baad mai)
+document.body.prepend(myH); - body mai insert krane ke liye ( phele )
+
+// add attributes in html tags/element dynamically - 
+input1.setAttribute('type','text');
+input1.setAttribute('name','nm');
+
+
+
+*/
+            
+
---
+++ DOM/handlingClassesUsingjs.js
@@ -0,0 +1,50 @@
+/*
+const lst = ele.classList;
+
+classList - element per jo jo classes assign hai , wo sab list ke ander milegi
+
+add(clsName) - to add class
+remove(clsName) -  to remove class
+replce(clsName1 , clsName2 ) - to replace class 1 to class 2 
+toggle(clsName) - class ko toggle krne ke liye
+
+*/
+
+
+// -------------------------------------------------------------------------------
+
+
+/*
+add class 
+
+const ele = document.getElementById('myH');
+console.log(ele);
+
+const lst = ele.classList;
+
+lst.add('clsA')
+lst.add('clsB')
+
+console.log(lst)
+
+*/
+
+
+
+/*
+apply class if not assigned 
+
+const ele = document.getElementById('myH');
+    console.log(ele);
+
+    const lst = ele.classList;
+
+    console.log( lst.contains('clsA') ) ;
+
+    if( !lst.contains('clsB')) {
+        lst.add('clsB');
+    }
+
+    console.log(lst)
+}
+*/
